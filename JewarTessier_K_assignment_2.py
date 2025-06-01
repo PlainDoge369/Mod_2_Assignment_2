d6dd7db3eaa921b4776406d0f8ad767cdb5fc040
kassius <kjewartessier@rrc.ca>
2025-05-31 19:40:55 -0500

    Mathematical Operators

--- JewarTessier_K_assignment_2.py
+++ JewarTessier_K_assignment_2.py
@@ -8,3 +8,15 @@ this_year = 2025
 print(f"this year: {this_year} type: {type(this_year)}")
 this_year += 1
 print(f"next year: {this_year} type: {type(this_year)}")
+
+# MATHEMATICAL OPERATIONS
+
+GST_RATE = 0.05 
+PST_RATE = 0.07
+vehicle_price = 88888.88
+provincial_tax = vehicle_price * PST_RATE
+federal_tax = vehicle_price * GST_RATE
+total_cost = vehicle_price + provincial_tax + federal_tax
+print("Purchase price:", vehicle_price, "Provincial Tax:", provincial_tax, "Federal Tax:", federal_tax, "Total:", total_cost)
+print(f"Purchase Price: ${vehicle_price:,.2f} Provincial Tax: ${provincial_tax:,.2f} Federal Tax: ${federal_tax:,.2f} Total: ${total_cost:,.2f}")
+
