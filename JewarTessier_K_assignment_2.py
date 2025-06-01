878feb716667a6fa20c4916141c0c2a512f6c089
kassius <kjewartessier@rrc.ca>
2025-05-31 19:33:15 -0500

    Simple Data Types

---
+++ JewarTessier_K_assignment_2.py
@@ -0,0 +1,10 @@
+# SIMPLE DATA TYPES
+
+name = "Kassius"
+print(f"name: {name} type: {type(name)}")
+has_license = False
+print(f"has license: {has_license} type: {type(has_license)}")
+this_year = 2025
+print(f"this year: {this_year} type: {type(this_year)}")
+this_year += 1
+print(f"next year: {this_year} type: {type(this_year)}")
